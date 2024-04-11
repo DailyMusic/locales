4fd85f2f544d05c405dfa5eb6fe58f17ac2d354b
arceus <96443442+blurwind@users.noreply.github.com>
2024-04-11 19:29:16 +0000

    fix: index

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-//
+module.exports = {
+    "en-US": {
+        "name": "English, US",
+        "nativeName": "English, US",
+        "status": "✅"
+    },
+    "es-ES": {
+        "name": "Spanish",
+        "nativeName": "Español",
+        "status": "✅"
+    },
+    "es-419": {
+        "name": "Spanish, LATAM",
+        "nativeName": "Español, LATAM",
+        "status": "✅"
+    },
+    "zh-CN": {
+        "name": "Chinese, China",
+        "nativeName": "中文",
+        "status": "✅"
+    },
+    "fr": {
+        "name": "French",
+        "nativeName": "Français",
+        "status": "✅"
+    },
+    "de": {
+        "name": "German",
+        "nativeName": "Deutsch",
+        "status": "✅"
+    },
+    "en-GB": {
+        "name": "English, UK",
+        "nativeName": "English, UK",
+        "status": "✅"
+    },
+    "ru": {
+        "name": "Russian",
+        "nativeName": "Pусский",
+        "status": "✅"
+    },
+    "tr": {
+        "name": "Turkish",
+        "nativeName": "Türkçe",
+        "status": "✅"
+    },
+    "pl": {
+        "name": "Polish",
+        "nativeName": "Polski",
+        "status": "✅"
+    },
+    "pt-BR": {
+        "name": "Portuguese, Brazilian",
+        "nativeName": "Português do Brasil",
+        "status": "✅"
+    }
+};
